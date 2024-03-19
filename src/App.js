decad7017ca5082d75fd44eb9db2c9aab1ca8612
frelosa <frelosa21@gmail.com>
2024-03-19 16:16:24 -0500

    clean project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState, useCallback } from "react";
+//import isEmpty from 'lodash/isEmpty';
+import { isEmpty } from "lodash";
+
+
 
 function App() {
+  const [task, setTask] = useState("");
+  
+  const addTask=(e)=>{
+    e.preventDefault();
+    if(isEmpty(task)){
+      console.log('vacio');
+      return;
+    }
+    console.log('Ok')
+    setTask("");
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1>Tareas</h1>
+      <hr />
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Lista de Tareas</h4>
+          <ul className="list-group">
+          <li className="list-group-item">
+            <span>Nombre de la tarea</span>
+            <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+            <button className="btn btn-warning btn-sm float-right">Editar</button>
+          </li>
+          </ul>
+          
+        </div>
+        <div className="col-4">
+          <h4 className="text-center">Formulario</h4>
+          <form onSubmit={addTask}>
+            <input type="text" className="form-control mb-2" placeholder="Ingresar la tarea..."
+            onChange={(text)=>setTask(text.target.value)}
+            value={task}
+            />
+            
+            <button className="btn btn-dark btn-block" type="submit">Agregar</button>
+
+          </form>
+        </div>
+
+      </div>
     </div>
   );
 }
